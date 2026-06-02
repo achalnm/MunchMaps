1adc621e55ea7702f1c5de5022a054d588306d46
achalnm <achalnm02@gmail.com>
2026-06-02 13:56:34 +0100

    update image paths in SQL files to use images/ prefix

--- database/migrate_add_imagefile.sql
+++ database/migrate_add_imagefile.sql
@@ -3,14 +3,14 @@
 
 ALTER TABLE `restaurant` ADD COLUMN `ImageFile` varchar(255) DEFAULT NULL;
 
-UPDATE `restaurant` SET `ImageFile` = 'DA.jpeg'                    WHERE RestaurantID = 1;
-UPDATE `restaurant` SET `ImageFile` = 'five star chicken.jpg'      WHERE RestaurantID = 2;
-UPDATE `restaurant` SET `ImageFile` = 'Masth cafe.jpg'             WHERE RestaurantID = 3;
-UPDATE `restaurant` SET `ImageFile` = 'nature coffee.jpg'          WHERE RestaurantID = 4;
-UPDATE `restaurant` SET `ImageFile` = 'royal cakes.jpg'            WHERE RestaurantID = 5;
-UPDATE `restaurant` SET `ImageFile` = 'SH.jpeg'                    WHERE RestaurantID = 6;
-UPDATE `restaurant` SET `ImageFile` = 'BC.jpeg'                    WHERE RestaurantID = 7;
-UPDATE `restaurant` SET `ImageFile` = 'GHC.jpeg'                   WHERE RestaurantID = 8;
-UPDATE `restaurant` SET `ImageFile` = 'lkbs.jpg'                   WHERE RestaurantID = 9;
-UPDATE `restaurant` SET `ImageFile` = 'vaishnavi Palace.jpg'       WHERE RestaurantID = 10;
-UPDATE `restaurant` SET `ImageFile` = 'vge gate.jpg'               WHERE RestaurantID = 11;
+UPDATE `restaurant` SET `ImageFile` = 'images/DA.jpeg'                    WHERE RestaurantID = 1;
+UPDATE `restaurant` SET `ImageFile` = 'images/five star chicken.jpg'      WHERE RestaurantID = 2;
+UPDATE `restaurant` SET `ImageFile` = 'images/Masth cafe.jpg'             WHERE RestaurantID = 3;
+UPDATE `restaurant` SET `ImageFile` = 'images/nature coffee.jpg'          WHERE RestaurantID = 4;
+UPDATE `restaurant` SET `ImageFile` = 'images/royal cakes.jpg'            WHERE RestaurantID = 5;
+UPDATE `restaurant` SET `ImageFile` = 'images/SH.jpeg'                    WHERE RestaurantID = 6;
+UPDATE `restaurant` SET `ImageFile` = 'images/BC.jpeg'                    WHERE RestaurantID = 7;
+UPDATE `restaurant` SET `ImageFile` = 'images/GHC.jpeg'                   WHERE RestaurantID = 8;
+UPDATE `restaurant` SET `ImageFile` = 'images/lkbs.jpg'                   WHERE RestaurantID = 9;
+UPDATE `restaurant` SET `ImageFile` = 'images/vaishnavi Palace.jpg'       WHERE RestaurantID = 10;
+UPDATE `restaurant` SET `ImageFile` = 'images/vge gate.jpg'               WHERE RestaurantID = 11;
